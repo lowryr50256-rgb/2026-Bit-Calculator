716b988468849dabb1a620b1200f7e3a4f3f7eb0
lowryr50256-rgb <lowryr50256@masseyhigh.school.nz>
2026-07-24 10:38:49 +1200

    Working statement generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# Functions go here 1 usage
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoraion * 5}")
+
+    # Main routine gose here
+    statement_generator("Instructions","-")
