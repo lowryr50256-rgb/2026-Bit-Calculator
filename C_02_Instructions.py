0a3ec848396217430aebecd03b66c3fc2839aae4
lowryr50256-rgb <lowryr50256@masseyhigh.school.nz>
2026-07-30 10:30:42 +1200

    working code

--- C_02_Instructions.py
+++ C_02_Instructions.py
@@ -7,8 +7,17 @@ def statement_generator (statement, decoration):
 def instructions():
     statement_generator("Instructions", "-")
 
+    print('''
+ Instructions go here.
+- instructions 1
+- instructions 2
+- etc
+    ''')
+
 
 # Main routine goes here
+
+# Display instructions if requested
 want_instructions = input("Press <enter> to read the instructions "
                            "or any key to continue ")
 
@@ -16,3 +25,4 @@ if want_instructions == "":
     instructions()
 
 print("program continues")
+
