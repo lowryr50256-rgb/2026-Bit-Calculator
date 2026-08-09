578ffaa8aa13db8ff178d728e6d587f2e10800a9
lowryr50256-rgb <lowryr50256@masseyhigh.school.nz>
2026-08-10 10:23:25 +1200

    filetype checker

--- c_03_File_Type.py
+++ c_03_File_Type.py
@@ -1,6 +1,38 @@
 # asks users for file type (integer / image / text / xxx) 1 usage
 def get_filetype():
-    pass
+
+    while True:
+        response = input("File type: ").lower()
+
+        # check  for 'i' or the exit code
+        if response == "xxx" or response == "i":
+            return response
+
+        # check if it's an integer
+        elif response in ['integer', 'int']:
+            return "integer"
+
+        # check for an image...
+        elif response in ['integer', 'picture', 'int']:
+            return "integer"
+
+        # check for text...
+        elif response in ['integer', 'int']:
+            return "text"
+
+        # if the response is invalid output an error
+        else:
+              print("Please enter a valid file type")
+
 
 #Main routaine gose here
-file_type = get_filetype()
+while True:
+    file_type = get_filetype()
+    print(f"you chose {file_type}")
+
+
+    if file_type == "xxx":
+        break
+
+
+        
