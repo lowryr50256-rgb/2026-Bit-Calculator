61d6bfa0cb6432d4fdbab98e392f12ce23712093
lowryr50256-rgb <lowryr50256@masseyhigh.school.nz>
2026-08-18 10:16:04 +1200

    working instructions and file Type checker

--- c_03_File_Type.py
+++ c_03_File_Type.py
@@ -1,33 +1,53 @@
 # asks users for file type (integer / image / text / xxx) 1 usage
 def get_filetype():
 
-    while True:
-        response = input("File type: ").lower()
 
-        # check  for 'i' or the exit code
-        if response == "xxx" or response == "i":
-            return response
+           while True:
+                response = input("File type: ").lower()
 
-        # check if it's an integer
-        elif response in ['integer', 'int']:
-            return "integer"
+                # check  for 'i' or the exit code
+                if response == "xxx" or response == "i":
+                    return response
 
-        # check for an image...
-        elif response in ['integer', 'picture', 'int']:
-            return "integer"
+                # check if it's an integer
+                elif response in ['integer', 'int']:
+                    return "integer"
 
-        # check for text...
-        elif response in ['integer', 'int']:
-            return "text"
+                # check for an image...
+                elif response in ['image', 'picture', 'int', 'p']:
+                    return "image"
 
-        # if the response is invalid output an error
-        else:
-              print("Please enter a valid file type")
+                # check for text...
+                elif response in ['text ', 'txt', 't']:
+                    return "text"
+
+                # if the response is invalid output an error
+                else:
+                      print("Please enter a valid file type")
 
 
-#Main routaine gose here
+# Main routine gose here
 while True:
     file_type = get_filetype()
+
+    # if user chose 'i',ask if they want an image / integer
+    if file_type =='i':
+
+        want_image = input("press <enter> for an integer or any other key for an image. ")
+
+        if want_image == "":
+            file_type = "integer"
+        else:
+            file_type = "image"
+
+    print(f"you chose {file_type}")
+
+    if file_type == "xxx":
+        break
+
+
+
+
     print(f"you chose {file_type}")
 
 
@@ -35,4 +55,3 @@ while True:
         break
 
 
-        
